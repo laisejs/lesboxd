783efefbd6b239e3a53c073005e274f94b045be1
laisejs <laisejschimithes@gmail.com>
2026-06-02 01:04:24 -0300

    pwa

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = 'lilies-cache-v1';
+const CACHE_NAME = 'lilies-cache-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/style.css',
+  '/manifest.json',
   '/lily.png',
   '/lily-light.png'
 ];
